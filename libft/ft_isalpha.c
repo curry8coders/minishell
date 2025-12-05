ae7f0424fea01ca0d0215e6bb19ac8dbededb969
hikaru <ichi-hikaru@i.softbank.jp>
2025-12-05 23:22:17 +0900

    modify the isalpha.c in libft folder.'

--- libft/ft_isalpha.c
+++ libft/ft_isalpha.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isalpha.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hichikaw <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: hichikaw <hichikaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 14:01:12 by hichikaw          #+#    #+#             */
-/*   Updated: 2024/11/16 01:31:38 by hichikaw         ###   ########.fr       */
+/*   Updated: 2025/12/05 23:21:42 by hichikaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,7 @@ int	ft_isalpha(int c)
 {
 	if ((64 < c && c < 91) || (96 < c && c < 123))
 	{
-		if (c % 2 == 0)
-			return (2);
-		else if (c % 2 == 1)
-			return (1);
-		else if (c % 3 == 0 && c % 2 == 0)
-			return (6);
-		else if (c % 3 == 0 && c % 2 == 1)
-			return (7);
+		return (1);
 	}
 	return (0);
 }
