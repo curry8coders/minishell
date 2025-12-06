60866387005c6a3cf0d3a6243e3a4564b6419093
hikaru <ichi-hikaru@i.softbank.jp>
2025-12-07 08:15:12 +0900

    be clear minishell.h file.

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ichikawahikaru <ichikawahikaru@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 16:58:40 by hichikaw          #+#    #+#             */
-/*   Updated: 2025/12/06 15:30:49 by ichikawahik      ###   ########.fr       */
+/*   Updated: 2025/12/06 19:54:21 by ichikawahik      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -108,68 +108,48 @@ struct s_shell
 	t_map			*envmap;
 };
 
-//expand_utils.c
+/* ************************************************************************** */
+/*                              TOKENIZER                                     */
+/* ************************************************************************** */
 
-// tokenize.c
+t_token				*tokenize(t_shell *shell, char *line);
+t_token				*new_token(char *word, t_token_kind kind);
+t_token				*operator(char **rest, char *line);
+t_token				*word(t_shell *shell, char **rest, char *line);
+char				**token_list_to_argv(t_token *tok);
+bool				is_blank(char c);
+bool				consume_blank(char **rest, char *line);
+bool				startswith(const char *s, const char *keyword);
+bool				is_metacharacter(char c);
+bool				is_word(const char *s);
 
-// tokenize_utils.c
+/* ************************************************************************** */
+/*                              EXPANDER                                      */
+/* ************************************************************************** */
 
-// expand.c
+void				expand(t_shell *shell, t_node *node);
+char				*expand_heredoc_line(t_shell *shell, char *line);
+void				expand_quote_removal(t_node *node);
+void				expand_variable_tok(t_shell *shell, t_token *tok);
+void				expand_variable_node(t_shell *shell, t_node *node);
+void				expand_variable(t_shell *sh, t_strbuf *buf,
+						char **rest, char *p);
+void				expand_special_param(t_shell *sh, t_strbuf *buf,
+						char **rest, char *p);
+void				remove_quote(t_token *tok);
+void				remove_single_quote(t_strbuf *buf, char **rest, char *p);
+void				remove_double_quote(t_strbuf *buf, char **rest, char *p);
+void				append_single_quote(t_strbuf *buf, char **rest, char *p);
+void				append_double_quote(t_shell *sh, t_strbuf *buf,
+						char **rest, char *p);
+bool				is_alpha_under(char c);
+bool				is_alpha_num_under(char c);
+bool				is_variable(char *s);
+bool				is_special_parameter(char *s);
 
-// destructor.c
-
-// parse.c
-
-// parse_redirect.c
-
-// parse_utils.c
-
-// parse_append.c
-
-// redirect.c
-
-// redirect_heredoc.c
-
-// redirect_open.c
-
-// pipe.c
-
-// exec.c
-
-// exec_utils.c
-char				*search_path(t_shell *shell, const char *filename);
-int					get_exit_status(int wstatus);
-char				*resolve_path(t_shell *shell, char **argv);
-void				handle_child_status(pid_t wait_result, pid_t last_pid,
-						int *status, int wstatus);
-
-// signal.c
-
-// signal_handler.c
-
-// builtin.c
-
-// builtin_exit.c
-
-// builtin_export.c
-
-// builtin_unset.c
-
-// builtin_env.c
-
-// builtin_cd.c
-
-// builtin_cd_utils.c
-
-// builtin_pwd.c
-
-// builtin_echo.c
-
-// map.c
-
-// map_utils.c
-
-// map_item.c
+/* ************************************************************************** */
+/*                            STRING BUFFER                                   */
+/* ************************************************************************** */
 
 void				strbuf_init(t_strbuf *buf);
 void				strbuf_grow(t_strbuf *buf, size_t needed);
@@ -177,53 +157,31 @@ void				strbuf_append_char(t_strbuf *buf, char c);
 void				strbuf_append_str(t_strbuf *buf, const char *s);
 char				*strbuf_finish(t_strbuf *buf);
 void				append_num(t_strbuf *buf, unsigned int num);
-void				expand_special_param(t_shell *sh, t_strbuf *buf,
-						char **rest, char *p);
-void				expand_variable(t_shell *sh, t_strbuf *buf,
-						char **rest, char *p);
-void				expand_variable_tok(t_shell *shell, t_token *tok);
-void				expand_variable_node(t_shell *shell, t_node *node);
-void				remove_single_quote(t_strbuf *buf, char **rest, char *p);
-void				remove_double_quote(t_strbuf *buf, char **rest, char *p);
-void				remove_quote(t_token *tok);
-void				append_single_quote(t_strbuf *buf, char **rest, char *p);
-void				append_double_quote(t_shell *sh, t_strbuf *buf,
-						char **rest, char *p);
-void				expand(t_shell *shell, t_node *node);
-char				*expand_heredoc_line(t_shell *shell, char *line);
-void				expand_quote_removal(t_node *node);
-bool				is_alpha_under(char c);
-bool				is_alpha_num_under(char c);
-bool				is_variable(char *s);
-bool				is_special_parameter(char *s);
-t_token				*tokenize(t_shell *shell, char *line);
-char				**token_list_to_argv(t_token *tok);
-t_token				*new_token(char *word, t_token_kind kind);
-t_token				*operator(char **rest, char *line);
-bool				is_blank(char c);
-bool				consume_blank(char **rest, char *line);
-bool				startswith(const char *s, const char *keyword);
-bool				is_metacharacter(char c);
-bool				is_word(const char *s);
-t_token				*word(t_shell *shell, char **rest, char *line);
-void				free_node(t_node *node);
-void				free_tok(t_token *tok);
-void				free_argv(char **argv);
+
+/* ************************************************************************** */
+/*                               PARSER                                       */
+/* ************************************************************************** */
+
 t_node				*parse(t_shell *shell, t_token *tok);
 t_node				*simple_command(t_shell *sh, t_token **rest, t_token *tok);
-bool				is_control_operator(t_token *tok);
 void				append_cmd_elm(t_shell *sh, t_node *cmd,
 						t_token **rest, t_token *tok);
-t_node				*redirect_out(t_token **rest, t_token *tok);
-t_node				*redirect_input(t_token **rest, t_token *tok);
-t_node				*redirect_append(t_token **rest, t_token *tok);
-t_node				*redirect_heredoc(t_token **rest, t_token *tok);
+bool				is_control_operator(t_token *tok);
 bool				at_eof(t_token *tok);
 bool				equal_op(t_token *tok, char *op);
 t_node				*new_node(t_node_kind kind);
 t_token				*tokdup(t_token *tok);
 void				append_tok(t_token **tokens, t_token *tok);
 void				append_node(t_node **node, t_node *elm);
+
+/* ************************************************************************** */
+/*                            REDIRECTION                                     */
+/* ************************************************************************** */
+
+t_node				*redirect_out(t_token **rest, t_token *tok);
+t_node				*redirect_input(t_token **rest, t_token *tok);
+t_node				*redirect_append(t_token **rest, t_token *tok);
+t_node				*redirect_heredoc(t_token **rest, t_token *tok);
 int					stashfd(int fd);
 bool				is_redirect(t_node *node);
 void				do_redirect(t_node *redirects);
@@ -234,10 +192,30 @@ void				close_pipeline_fds_except_current(t_node *h, t_node *cur);
 int					read_heredoc(t_shell *sh, const char *delim,
 						bool is_delim_unquoted);
 int					open_redir_file(t_shell *shell, t_node *node);
+
+/* ************************************************************************** */
+/*                               PIPELINE                                     */
+/* ************************************************************************** */
+
 int					prepare_pipe(t_node *node);
 void				prepare_pipe_child(t_node *node);
 void				prepare_pipe_parent(t_node *node);
+
+/* ************************************************************************** */
+/*                              EXECUTOR                                      */
+/* ************************************************************************** */
+
 int					exec(t_shell *shell, t_node *node);
+char				*search_path(t_shell *shell, const char *filename);
+int					get_exit_status(int wstatus);
+char				*resolve_path(t_shell *shell, char **argv);
+void				handle_child_status(pid_t wait_result, pid_t last_pid,
+						int *status, int wstatus);
+
+/* ************************************************************************** */
+/*                               SIGNALS                                      */
+/* ************************************************************************** */
+
 int					check_state(void);
 void				setup_signal(t_shell *shell);
 void				reset_signal(void);
@@ -245,31 +223,47 @@ void				handler(int signum);
 void				reset_sig(int signum);
 void				ignore_sig(int signum);
 void				setup_sigint(void);
+
+/* ************************************************************************** */
+/*                              BUILTINS                                      */
+/* ************************************************************************** */
+
 bool				is_builtin(t_node *node);
 int					exec_builtin(t_shell *shell, t_node *node);
-bool				is_numeric(char *s);
-int					builtin_exit(t_shell *shell, char **argv);
+int					builtin_echo(char **argv);
+int					builtin_cd(t_shell *shell, char **argv);
+int					builtin_pwd(t_shell *shell, char **argv);
 int					builtin_export(t_shell *shell, char **argv);
 int					builtin_unset(t_shell *shell, char **argv);
 int					builtin_env(t_shell *shell, char **argv);
-int					builtin_cd(t_shell *shell, char **argv);
+int					builtin_exit(t_shell *shell, char **argv);
+
+/* builtin_echo */
+bool				check_only_n_options(char *arg);
+
+/* builtin_cd */
 void				delete_last_elm(char *path);
 void				append_path_elm(char *dst, char **rest, char *src);
 void				process_path_element(char *newpwd, char **path);
 char				*resolve_pwd(char *oldpwd, char *path);
-int					builtin_pwd(t_shell *shell, char **argv);
-bool				check_only_n_options(char *arg);
-int					builtin_echo(char **argv);
+
+/* builtin_exit */
+bool				is_numeric(char *s);
+
+/* ************************************************************************** */
+/*                          ENVIRONMENT MAP                                   */
+/* ************************************************************************** */
+
 t_map				*map_new(void);
 char				*map_get(t_map *map, const char *name);
 int					map_set(t_map *map, const char *name, const char *value);
 int					map_unset(t_map *map, const char *name);
-bool				is_identifier(const char *s);
-t_item				*item_new(char *name, char *value);
-char				*item_get_string(t_item *item);
 int					map_put(t_map *map, const char *str, bool allow_empty);
 size_t				map_len(t_map *map, bool count_null_value);
 void				map_printall(t_map *map);
+bool				is_identifier(const char *s);
+t_item				*item_new(char *name, char *value);
+char				*item_get_string(t_item *item);
 void				update_existing_item(t_item *cur, const char *value);
 t_item				*create_new_item(const char *name, const char *value);
 void				parse_name_value(const char *str, char **name, char **val);
@@ -277,7 +271,10 @@ char				*xgetenv(t_shell *shell, const char *name);
 void				initenv(t_shell *shell);
 char				**get_environ(t_map *map);
 
-//error.c
+/* ************************************************************************** */
+/*                            ERROR HANDLING                                  */
+/* ************************************************************************** */
+
 void				perror_prefix(void);
 void				fatal_error(const char *msg) __attribute__((noreturn));
 void				assert_error(const char *msg) __attribute__((noreturn));
@@ -286,12 +283,18 @@ void				err_exit(const char *loc, const char *msg, int status)
 					__attribute__((noreturn));
 void				command_not_found_error(const char *location);
 void				permission_denied_error(const char *location);
-
-//error_syntax.c
 void				tokenize_error(t_shell *sh, const char *loc,
 						char **rest, char *line);
 void				parse_error(t_shell *shell, t_token **rest, t_token *tok);
 void				xperror(const char *location);
 void				builtin_error(const char *f, const char *n, const char *e);
+
+/* ************************************************************************** */
+/*                             DESTRUCTOR                                     */
+/* ************************************************************************** */
+
+void				free_node(t_node *node);
+void				free_tok(t_token *tok);
+void				free_argv(char **argv);
 
 #endif
