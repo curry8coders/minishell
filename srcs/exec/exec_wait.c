5d723e6b2013d704bc262970774dcde0a2476f27
hikaru <ichi-hikaru@i.softbank.jp>
2025-12-10 13:04:52 +0900

    fix signal hander.

--- srcs/exec/exec_wait.c
+++ srcs/exec/exec_wait.c
@@ -6,7 +6,7 @@
 /*   By: ichikawahikaru <ichikawahikaru@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/06 02:50:00 by ichikawahik       #+#    #+#             */
-/*   Updated: 2025/12/06 02:50:00 by ichikawahik      ###   ########.fr       */
+/*   Updated: 2025/12/10 12:55:00 by agent           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,16 @@
 void	handle_child_status(pid_t wait_result, pid_t last_pid,
 		int *status, int wstatus)
 {
+	static int	signaled_newline_printed;
+
 	if (wait_result == last_pid)
-	{
+		signaled_newline_printed = 0;
+	if (wait_result == last_pid)
 		*status = get_exit_status(wstatus);
-		if (WIFSIGNALED(wstatus))
-			write(STDERR_FILENO, "\n", 1);
+	if (WIFSIGNALED(wstatus) && !signaled_newline_printed)
+	{
+		write(STDERR_FILENO, "\n", 1);
+		signaled_newline_printed = 1;
 	}
 }
+
